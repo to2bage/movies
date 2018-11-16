964ec607b2796df48ca33efba801393fea9a81be
to2bage <to2bage@hotmail.com>
2018-11-16 15:43:25 +0800

    数据填充页面初步

--- src/components/movie/MovieItem.jsx
+++ src/components/movie/MovieItem.jsx
@@ -7,7 +7,12 @@ class MovieItem extends Component {
     }
     render() { 
         return ( 
-            <div></div>
+            <div>
+                <img src={this.props.images.small} />
+                <h4>电影名称: {this.props.title}</h4>
+                <h4>电影类型: {this.props.genres.join(", ")}</h4>
+                <h4>上映年份: {this.props.year}年</h4>
+            </div>
          );
     }
 }
--- src/components/movie/MovieList.jsx
+++ src/components/movie/MovieList.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-
+// 导入自定义组件
+import MovieItem from '@/components/movie/MovieItem.jsx';
 // 导入跨域使用的第三方fetch组件
 import fetchJSONP from 'fetch-jsonp';
+// 导入ant-design组件
+import { Spin } from 'antd';
+
 
 class MovieList extends Component {
     constructor(props) {
@@ -9,21 +13,57 @@ class MovieList extends Component {
         this.state = { 
             nowPage: 1,
             pageSize: 12,
-
+            movies: [],
+            isLoading: true
         }
+
+        this.renderMovieInfo = this.renderMovieInfo.bind(this);
     }
 
     componentWillReceiveProps(nextProps) {
         // console.log(nextProps.match.params);
         const url = `http://api.douban.com/v2/movie/${nextProps.match.params.type}?start=0&count=${this.state.pageSize}`;
         // console.log(url);
-        fetchJSONP(url).then(resp => resp.json()).then(data => console.log(data));
+        fetchJSONP(url)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data);
+                this.setState(() => {
+                    // 获得数据后
+                    return {
+                        isLoading: false,    // 是否在转载状态设为false
+                        movies: data.subjects,  // 当前页的电影信息的数组
+                    }
+                }, () => {
+                    //this.renderMovieInfo();
+                    console.log(this.state.movies);
+                })
+            });
     }
 
     render() { 
+        console.log("assert render()");
         return ( 
-            <div>{this.props.match.params.type} --- {this.props.match.params.page}</div>
+            <div>
+                {this.renderMovieInfo()}
+            </div>
          );
+    }
+
+    renderMovieInfo() {
+        if (this.state.isLoading) {
+            return <Spin size="large" />
+        } else {
+            return (
+                <div>
+                    {
+                        this.state.movies.map((movie) => {
+                            return <MovieItem {...movie} key={movie.id}></MovieItem>
+                        })
+                    }
+                </div>
+            );
+        }
     }
 }
  
